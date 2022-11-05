5a7014381b67123eadf810e437475502238bb8e5
irajyus <irach.yusufiy@getnacelle.com>
2022-11-05 05:57:28 -0400

    add crypto

--- api/index.js
+++ api/index.js
@@ -3,6 +3,13 @@ export default async function handler(req, res) {
   try {
     const body = await req.body;
     const rawBody = JSON.stringify(body);
+    const xvs = req.get("x-vercel-signature");
+    const signature = crypto
+      .createHmac("sha1", process.env.OAUTH2_SECRET)
+      .update(rawBody)
+      .digest("hex");
+    console.log(xvs);
+    console.log(signature);
     console.log(rawBody);
     console.log(typeof rawBody);
     console.log(req.headers);
