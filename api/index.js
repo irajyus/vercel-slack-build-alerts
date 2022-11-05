c727553452c11722e1816a58c62396b21bf72bc2
irajyus <irach.yusufiy@getnacelle.com>
2022-11-05 05:44:16 -0400

    stringify

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
-export const config = { api: { bodyParser: false } };
-
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(404).end();
   try {
     const body = await req.body;
-    const rawBody = JSON.parse(JSON.stringify(body));
+    const rawBody = JSON.stringify(body);
     console.log(rawBody);
     console.log(typeof rawBody);
     console.log(req.headers);
