8d5c432d86fdf5719692a6cfa2737dc2b129b983
irajyus <irach.yusufiy@getnacelle.com>
2022-11-05 07:57:16 -0400

    switch to axios

--- api/_utils.js
+++ api/_utils.js
@@ -1,28 +1,26 @@
 import crypto from "crypto";
-import fetch from "node-fetch";
+const axios = require("axios");
 
 export function createSig(rawBody, secret) {
   return crypto.createHmac("sha1", secret).update(rawBody).digest("hex");
 }
 
 export async function postToSlack(webhookURL, messageBody) {
-  try {
-    const response = await fetch(webhookURL, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(messageBody),
+  axios({
+    method: "post",
+    url: webhookURL,
+    headers: { "Content-Type": "application/json" },
+    data: JSON.stringify(messageBody),
+  })
+    .then((response) => {
+      console.log("data from axios:", response.data);
+      res.json({ ok: true });
+    })
+    .catch((err) => {
+      console.log("axios Error:", err);
+      res.send({
+        response_type: "ephemeral",
+        text: `${err.response.data.error}`,
+      });
     });
-    const data = await response.json();
-
-    console.log("data from fetch:", data);
-    res.json({ ok: true });
-  } catch (err) {
-    console.log("fetch Error:", err);
-    res.send({
-      response_type: "ephemeral",
-      text: `${err}`,
-    });
-  }
 }
