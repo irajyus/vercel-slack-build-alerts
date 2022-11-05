302ff18c206fa1f9e91c14d4ce0d1b623776b6ec
irajyus <irach.yusufiy@getnacelle.com>
2022-11-05 07:24:00 -0400

    update console.log order

--- api/index.js
+++ api/index.js
@@ -8,15 +8,15 @@ export default async function handler(req, res) {
     const rawBody = JSON.stringify(body);
     const signature = createSig(rawBody, secret);
     const xvs = req.headers["x-vercel-signature"];
+    console.log(xvs);
+    console.log(signature);
+    console.log(rawBody);
+    console.log(typeof rawBody);
     if (signature !== xvs) {
       return res.status(403).end();
     } else {
       console.log("Signature matched");
     }
-    console.log(xvs);
-    console.log(signature);
-    console.log(rawBody);
-    console.log(typeof rawBody);
   } catch (error) {
     console.log(error.message);
   } finally {
