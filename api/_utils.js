007a0cbde837448e56326aceda8fe2e4ac4eac90
irajyus <irach.yusufiy@getnacelle.com>
2022-11-05 07:10:01 -0400

    update require/import

--- api/_utils.js
+++ api/_utils.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-const crypto = require("crypto");
+import crypto from "crypto";
 
 export async function createSig(rawBody, secret) {
   return crypto.createHmac("sha1", secret).update(rawBody).digest("hex");
