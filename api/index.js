b96b672598fdaad49e1241f752a02adbf539e48e
irajyus <irach.yusufiy@getnacelle.com>
2022-11-05 05:34:16 -0400

    update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
 export default async function handler(req, res) {
-  const { body } = req;
-  console.log(body);
-  return res.body;
+  if (req.method !== "POST") return res.status(404).end();
+  try {
+    const rawBody = getRawBody(req);
+    console.log(rawBody);
+    console.log(req.headers);
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    res.json({ received: true });
+  }
+}
+
+function getRawBody(req) {
+  return new Promise((resolve, reject) => {
+    let bodyChunks = [];
+    req.on("end", () => {
+      const rawBody = Buffer.concat(bodyChunks).toString("utf8");
+      resolve(rawBody);
+    });
+    req.on("data", (chunk) => bodyChunks.push(chunk));
+  });
 }
