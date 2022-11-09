398ff4a4e55bef317dbfbdb0f493f7afc8e47de3
irajyus <irach.yusufiy@getnacelle.com>
2022-11-09 16:13:42 -0500

    update

--- api/index.js
+++ api/index.js
@@ -42,8 +42,8 @@ export default async function handler(req, res) {
     }
   } catch (error) {
     console.log(error.message);
-    return res.status(400).json({ error });
   }
+  return res.status(200).send("OK");
   // finally {
   //   return res.status(200).send("OK");
   // }
